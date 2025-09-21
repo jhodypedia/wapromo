78e7ebec91017bb019fc8a18ec904dbfe9ec7232
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-21 16:04:07 +0700

    Update Template.js

--- models/Template.js
+++ models/Template.js
@@ -1,9 +1,12 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/db.js";
+
 const Template = sequelize.define("Template", {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   title: DataTypes.STRING,
-  body: DataTypes.TEXT,     // tanpa link
-  link: DataTypes.STRING,   // link dipisah
+  body: DataTypes.TEXT,    // pesan utama
+  link: DataTypes.STRING,  // link opsional
   isActive: { type: DataTypes.BOOLEAN, defaultValue: true }
 });
+
 export default Template;
