8cd8280aecfa44358fa74c2eca56fcdd027b5121
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-22 06:08:48 +0700

    Update generate.controller.js

--- controllers/generate.controller.js
+++ controllers/generate.controller.js
@@ -1,12 +1,11 @@
-import { generateNumbers } from "../services/numberGenerator.js"; // fungsi yg kamu buat (sudah dimodif prefix 62)
+import { generateNumbers, operatorPrefixes } from "../services/numberGenerator.js";
 
 /**
- * Generate nomor (AJAX)
+ * API: generate nomor (balik JSON untuk AJAX)
  */
 export const generate = async (req, res) => {
   try {
     const { operator, count, length } = req.body;
-
     if (!operator || !count || !length) {
       return res.status(400).json({ success: false, error: "Data tidak lengkap" });
     }
@@ -21,7 +20,6 @@ export const generate = async (req, res) => {
 /**
  * Render halaman generate (EJS)
  */
-export const renderGeneratePage = async (req, res) => {
-  const operators = Object.keys((await import("../utils/generator.js")).operatorPrefixes);
-  res.render("generate/index", { operators });
+export const renderGeneratePage = (req, res) => {
+  res.render("generate/index", { operators: Object.keys(operatorPrefixes) });
 };
