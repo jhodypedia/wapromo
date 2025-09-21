6e630357d116cdee5c8d4a9a023fbf0f7b1f81ef
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-21 16:04:26 +0700

    Update Session.js

--- models/Session.js
+++ models/Session.js
@@ -1,8 +1,11 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/db.js";
+
 const Session = sequelize.define("Session", {
-  sessionId: { type: DataTypes.STRING, unique: true },
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  sessionId: { type: DataTypes.STRING, unique: true }, // id eksternal WA
   label: DataTypes.STRING,
   status: { type: DataTypes.STRING, defaultValue: "disconnected" } // connected|reconnecting|disconnected
 });
+
 export default Session;
