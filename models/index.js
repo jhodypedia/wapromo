5503e8092fe1fd7a6c922b1863e6b061f7d30436
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-21 09:24:43 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -5,8 +5,16 @@ import Session from "./Session.js";
 import Campaign from "./Campaign.js";
 import Target from "./Target.js";
 
-Campaign.belongsTo(User, { foreignKey: "userId" });
-Campaign.belongsTo(Template, { foreignKey: "templateId" });
-Target.belongsTo(Campaign, { foreignKey: "campaignId" });
+// Campaign milik User & Template
+Campaign.belongsTo(User, { foreignKey: "userId", as: "user" });
+Campaign.belongsTo(Template, { foreignKey: "templateId", as: "template" });
+
+// Campaign punya banyak Target
+Campaign.hasMany(Target, { foreignKey: "campaignId", as: "targets" });
+Target.belongsTo(Campaign, { foreignKey: "campaignId", as: "campaign" });
+
+// (opsional) kalau campaign terhubung ke session
+Campaign.belongsTo(Session, { foreignKey: "sessionId", as: "session" });
+Session.hasMany(Campaign, { foreignKey: "sessionId", as: "campaigns" });
 
 export { sequelize, User, Template, Session, Campaign, Target };
