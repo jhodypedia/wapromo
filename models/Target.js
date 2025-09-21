e4a42060af6574341e0da5df2e61a4b8bd4af731
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-21 16:05:10 +0700

    Update Target.js

--- models/Target.js
+++ models/Target.js
@@ -1,9 +1,15 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/db.js";
+
 const Target = sequelize.define("Target", {
-  campaignId: DataTypes.INTEGER,
-  number: DataTypes.STRING,                   // 62xxxx
-  status: { type: DataTypes.STRING, defaultValue: "pending" }, // pending|valid|invalid|success|error
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  campaignId: { type: DataTypes.INTEGER, allowNull: false },
+  number: DataTypes.STRING,  // 62xxxx
+  status: { 
+    type: DataTypes.ENUM("pending", "valid", "invalid", "success", "error"),
+    defaultValue: "pending"
+  },
   error: DataTypes.TEXT
 });
+
 export default Target;
