2d00d2c3e67d6903a3f437be159e15c0a16477eb
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-22 07:34:32 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,32 @@
-// models/user.js
+// models/User.js
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/db.js";
 
 const User = sequelize.define("User", {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false }
+  id: { 
+    type: DataTypes.INTEGER, 
+    autoIncrement: true, 
+    primaryKey: true 
+  },
+  name: { 
+    type: DataTypes.STRING, 
+    allowNull: false 
+  },
+  email: { 
+    type: DataTypes.STRING, 
+    allowNull: false,       // 🔑 jangan boleh null
+    unique: true,           // 🔑 pastikan unik
+    validate: {
+      isEmail: true         // 🔑 validasi format email
+    }
+  },
+  password: { 
+    type: DataTypes.STRING, 
+    allowNull: false 
+  }
+}, {
+  tableName: "users",       // biar jelas nama tabelnya
+  timestamps: true          // otomatis bikin createdAt & updatedAt
 });
 
 export default User;
